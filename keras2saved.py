652f9ac503093586385f1cb00c7cfcfd4f0e422b
coolseaweed <jeellato@gmail.com>
2022-08-09 00:11:28 +0900

    add keras2saved script

--- keras2saved.py
+++ keras2saved.py
@@ -9,103 +9,35 @@ from tensorflow_tts.inference import TFAutoModel
 from tensorflow_tts.inference import AutoProcessor
 from tensorflow_tts.inference import AutoConfig
 
+from glob import glob
+
 
 def get_args():
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--text2mel-path',
-        dest="text2mel_path",
+        '--input',
         default="models/fastspeech2/v1/model-200000.h5",
-        help='text2mel model path'
-    )
-
-    parser.add_argument(
-        '--text2mel-config',
-        dest="text2mel_config",
-        default="models/fastspeech2/v1/conf.yaml",
-        help='text2mel config path'
-    )
-
-    parser.add_argument(
-        '--text2mel-name',
-        dest="text2mel_name",
-        default="fastspeech2",
-        help='text2mel config path'
-    )
-
-    parser.add_argument(
-        '--vocoder-path',
-        dest="vocoder_path",
-        default="models/tf/hifigan/generator-81250.h5",
-        help='vocoder model path'
-    )
-
-    parser.add_argument(
-        '--vocoder-config',
-        dest="vocoder_config",
-        default="./models/tf/hifigan/conf.yaml",
-        help='text2mel config path'
-    )
-    parser.add_argument(
-        '--vocoder-name',
-        dest="vocoder_name",
-        default="hifi_gan",
-        help='text2mel config path'
+        help='input keras dir'
     )
 
     # parser.add_argument(
-    #     'input',
-    #     default='input.txt',
-    #     help='input text file'
+    #     '--name',
+    #     default="models/fastspeech2/v1/model-200000.h5",
+    #     choices=['hifi_gan', 'fastspeech2', 'multib'],
+    #     help='model dir'
     # )
 
-    # parser.add_argument(
-    #     'output',
-    #     default='output',
-    #     help='out audio dir'
-    # )
+    parser.add_argument(
+        '--output',
+        default="output",
+        help='output tensorflow saved dir'
+    )
 
     args = parser.parse_args()
 
     return args
-
-
-def do_synthesis(input_text, text2mel_model, vocoder_model, text2mel_name, vocoder_name, processor):
-    input_ids = processor.text_to_sequence(input_text)
-
-    # text2mel part
-    if text2mel_name == "TACOTRON":
-        _, mel_outputs, stop_token_prediction, alignment_history = text2mel_model.inference(
-            tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
-            tf.convert_to_tensor([len(input_ids)], tf.int32),
-            tf.convert_to_tensor([0], dtype=tf.int32)
-        )
-    elif text2mel_name == "FASTSPEECH2":
-        mel_before, mel_outputs, duration_outputs, _, _ = text2mel_model.inference(
-            tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
-            speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
-            speed_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
-            f0_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
-            energy_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
-        )
-    else:
-        raise ValueError("Only TACOTRON, FASTSPEECH2 are supported on text2mel_name")
-
-    print(f"mel output: {mel_outputs.shape} / type: {type(mel_outputs)}")
-    # vocoder part
-    if vocoder_name == "MB_MELGAN":
-        audio = vocoder_model.inference(mel_outputs)[0, :, 0]
-    elif vocoder_name == "HIFI_GAN":
-        audio = vocoder_model.inference(mel_outputs)[0, :, 0]
-    else:
-        raise ValueError("Only MB_MELGAN are supported on vocoder_name")
-
-    if text2mel_name == "TACOTRON":
-        return mel_outputs.numpy(), alignment_history.numpy(), audio.numpy()
-    else:
-        return mel_outputs.numpy(), audio.numpy()
 
 
 def check_params(arg):
@@ -113,7 +45,7 @@ def check_params(arg):
     print(" ------- PARAMS ------- ")
     for key, val in arg_dict.items():
         print(f"* {key}: {val}")
-    print(" ------- ------- ------- ")
+    print(" ----------------------- ")
 
 
 def main():
@@ -121,19 +53,27 @@ def main():
     args = get_args()
     check_params(args)
 
-    # input_text = args.input
-    # output_dir = args.output
+    input_dir = args.input
+    output_dir = args.output
 
-    # initialize melgan model
-    vocoder_config = AutoConfig.from_pretrained(args.vocoder_config)
-    vocoder_name = args.vocoder_name
-    vocoder = TFAutoModel.from_pretrained(
-        config=vocoder_config,
-        pretrained_path=args.vocoder_path,
-        name=vocoder_name
+    model_list = sorted(glob(f"{input_dir}/*.h5"))
+    assert len(model_list) != 0, f"There is no model file in {input_dir}"
+    model_path = model_list[-1]
+
+    config_list = [file for file in glob(f"{input_dir}/*") if "yaml" in file or 'yml' in file]
+    assert len(config_list) != 0, f"There is no config file in {input_dir}"
+    config_path = config_list[-1]
+
+    # initialize model
+    config = AutoConfig.from_pretrained(config_path)
+
+    model = TFAutoModel.from_pretrained(
+        config=config,
+        pretrained_path=model_path,
     )
-    vocoder.save('./models/test_1')
-    print(type(vocoder))
+    model.save(output_dir)
+    print(f"PARAMs CHECK\n* model_path: {model_path}\n* config_path: {config_path}")
+    print(f"DONE !")
 
 
 if __name__ == '__main__':
